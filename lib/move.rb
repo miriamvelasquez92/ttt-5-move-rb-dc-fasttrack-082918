34e198523923a1bd340565e3cc326c861cca9709
Miriam Velasquez <vbmiriamg@gmail.com>
2018-09-30 16:17:22 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,8 +12,5 @@ def input_to_index(user_input)
 end
 
 def move(board, index, character= "X")
-  puts "the board is #{board}"
-  puts "the position is #{index}"
-  puts "the character is #{character}"
   board[index] = character
 end
